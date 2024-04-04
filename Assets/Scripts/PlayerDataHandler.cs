2c7e8220082b04516a1aa3e41725822dc93a8826
Teebor <gavinpowell@btinternet.com>
2024-04-04 16:15:13 +0100

    Main Menu added
    Created new main menu and simple scripts to start handling persistent data

---
+++ Assets/Scripts/PlayerDataHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDataHandler : MonoBehaviour
+{
+    public static PlayerDataHandler Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake() {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
